4fe2f28df2cb5b426bd79f80560ed525ba2f929f
Mykhailo Mirzaiev <michaelmirzaev@gmail.com>
2026-03-30 11:37:34 +0300

    Initial Commit

---
+++ Antistress/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Antistress
+//
+//  Created by Mykhailo Mirzaiev on 30.03.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
